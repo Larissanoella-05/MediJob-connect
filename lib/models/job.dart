961b413eb4bff732f6305441a037dd4928f30721
TWA-Coder <twakaru744@gmail.com>
2025-11-08 12:00:00 +0200

    Backdated commit 8: job.dart update 2

---
+++ lib/models/job.dart
@@ -0,0 +1,15 @@
+class Job {
+  final String id;
+  final String title;
+  final String description;
+  final String companyName;
+  final DateTime postedAt;
+
+  Job({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.companyName,
+    required this.postedAt,
+  });
+}
